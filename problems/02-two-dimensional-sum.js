dfd6e64ea06a3866e86048349418f09972fa263c
jnorm23 <joshua_norman11@hotmail.com>
2024-06-20 21:01:44 -0400

    edit

--- problems/02-two-dimensional-sum.js
+++ problems/02-two-dimensional-sum.js
@@ -5,10 +5,10 @@ returns the total sum of all numbers.
 
 // Your code here 
 function twoDimensionalSum(arr) {
-    let total = 0;   //initializing a variable 'total' to 0. 'total' will be the sum of all elements in the 2D array
-    for (let i = 0; i < arr.length; i++) {    //outer loop iterates over each subarray in the 2D array
+    let total = 0;                                //initializing a variable 'total' to 0. 'total' will be the sum of all elements in the 2D array
+    for (let i = 0; i < arr.length; i++) {        //outer loop iterates over each subarray in the 2D array
       for (let j = 0; j < arr[i].length; j++) {   //inner loop iterates over each element in the current subarray 
-        total += arr[i][j];    //in each iteration of the inner loop, the current element 'arr[i][j]' is added to 'total'
+        total += arr[i][j];                       //in each iteration of the inner loop, the current element 'arr[i][j]' is added to 'total'
       }
     }
     return total;  //return the totalSum after all elements have been processed
